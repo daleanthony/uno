4f115bfaf0ae4d4026c4831083c78ac5f1846441
Dale Williams <dale@daleanthony.com>
2014-02-10 17:50:50 +0000

    Fixed issue with side panel overlapping content in older browsers

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
   $('body').removeClass('no-js');
 
   $('a.blog-button').click(function() {
-    if ($('.panel-main').hasClass('panel-main--collapsed')) return;
-    currentWidth = $('.panel-main').width();
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
-      $('.panel-main').addClass('panel-main--collapsed');
+      $('.panel-cover').addClass('panel-cover--collapsed');
       $('.content-wrapper').addClass('animated slideInRight');
     } else {
-      $('.panel-main').css('max-width',currentWidth);
-      $('.panel-main').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').css('max-width',currentWidth);
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function() {} );
     }
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
-    $('.panel-main').addClass('panel-main--collapsed');
+    $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
   $('.btn-mobile-menu').click(function() {
